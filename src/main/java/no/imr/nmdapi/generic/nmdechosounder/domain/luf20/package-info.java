882192bd2fd068bbde9c7f7e5ad28247457b66c9
KjetilFjellheim <kjetil.fjellheim@imr.no>
2015-08-05 09:24:34 +0200

    Inital commit

---
+++ src/main/java/no/imr/nmdapi/generic/nmdechosounder/domain/luf20/package-info.java
@@ -0,0 +1,4 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.imr.no/formats/nmdechosounder/v1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = {
+    @javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.imr.no/formats/nmdechosounder/v1", prefix = "")
+})
+package no.imr.nmdapi.generic.nmdechosounder.domain.luf20;
